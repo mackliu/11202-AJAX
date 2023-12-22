e7c900f35423de421ca5e29d4dbc0c2cf288ffe3
kai <cgk0910@gmail.com>
2023-12-22 16:05:45 +0800

    使用ajax 來進行條件較為複雜的查詢

--- app2/api/query.php
+++ app2/api/query.php
@@ -8,13 +8,32 @@ switch($_GET['do']){
     break;
     case 'sex':
         $users=$Student->q("select `name`,`uni_id`,`school_num`,`birthday` from `students` where substr(`uni_id`,2,1)='{$_GET['value']}'");
-        
+
         header('Content-Type: application/json; charset=utf-8');        
         echo json_encode($users);
     break;
     case 'class':
+        
+        $stnums=$ClassStudent->all(['class_code'=>$_GET['value']]);
+        //dd($stnums);
+        $nums=[];
+        foreach($stnums as $st){
+            $s=$Student->find(['school_num'=>$st['school_num']]);
+            if(!empty($s)){
+                $nums[]=$s['id'];
+            }
+        }
+        $in=join(',',$nums);
+        $users=$Student->q("select `name`,`uni_id`,`school_num`,`birthday` from `students` where `id` in($in)");
+
+        header('Content-Type: application/json; charset=utf-8');
+        echo json_encode($users);
             
-            dd($_GET['value']);
+    break;
+    case 'classes':
+        $classes=$Class->q("select `code`,`name` from `classes`");
+        header('Content-Type: application/json; charset=utf-8');        
+        echo json_encode($classes);
     break;
 }
 
